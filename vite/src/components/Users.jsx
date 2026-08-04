4061c085f38142ae293da59f838d9bd25bc1aabf
ByKimhy <kimhyby99@gmail.com>
2026-08-04 10:59:36 +0700

    find vs filter

--- vite/src/components/Users.jsx
+++ vite/src/components/Users.jsx
@@ -20,7 +20,12 @@ const UserData = [
 ];
 
 function getIdUser(id) {
-    console.log("Clicked id:", id);
+    // console.log("Clicked id:", id);
+
+    // noted: find method returns the first element that matches the condition. example: { id: 1, name: "Kimhy", position: "Frontend Developer" }
+    // noted: filter method returns an array of all elements that match the condition example: [{ id: 1, name: "Kimhy", position: "Frontend Developer" }, { id: 2, name: "Alice", position: "Frontend Developer" }]
+    const newUser = UserData.find(user => user.id === id);
+    console.log(newUser);
 }
 
 const Users = () => {
