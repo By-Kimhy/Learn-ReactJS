c28eebab17deb837e8b4546cdb4b78a3b9d17e77
ByKimhy <kimhyby99@gmail.com>
2026-08-04 14:40:17 +0700

    update alert

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import UserssList from './components/UserssList'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
+  const [alert, setAlert] = useState(false);
+  function handleToggle() {
+    // if (alert) {
+    //   setAlert(false);
+    // } else {
+    //   setAlert(true);
+    // }
+
+    setAlert(!alert);
+  }
   return (
-    <UserssList />
-  )
+    <div>
+      {alert ? <Message /> : null}
+      <button onClick={handleToggle}>Toggle Alert</button>
+    </div>
+  );
+};
+
+function Message() {
+  return <h2>Message...</h2>;
 }
 
-export default App
+export default App;
