969473d8920d4aeeec8b959ecd5eadbb68f63bbe
ByKimhy <kimhyby99@gmail.com>
2026-08-05 18:07:13 +0700

    Route

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import {Routes, Route,Link} from "react-router-dom";
+import "./App.css";
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  ) 
+}
+function AboutPage() {
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  ) 
+}
+function ErrorPage() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+    </div>
+  ) 
+}
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <nav>
+        <Link>Home</Link>
+        <Link>About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </div>
   )
 }
 
